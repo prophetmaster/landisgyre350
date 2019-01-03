30748b854358f0ae61608164c97f8322bd3f8ef0
Buchs Sullivan <sullivan.buchs@gmail.com>
2019-01-03 19:25:25 +0100

    modified the source

--- landisgyr_mysensors/landisgyr_mysensors.ino
+++ landisgyr_mysensors/landisgyr_mysensors.ino
@@ -1,8 +1,17 @@
-#define MY_DEBUG 
-// Define Node ID
+/*	Landis & Gyr E350
+
+	Read electric meter with IR serial and send to MySensors gateway
+
+	Buchs Sullivan
+*/
+
+//#define MY_DEBUG 
+
+// Optional define Node ID
 #define MY_NODE_ID 27
+
+// Define radio for MySensors
 #define MY_RADIO_NRF24
-#define MY_BAUD_RATE (9600ul)
 #define MY_SOFTSPI
 #define MY_RF24_CE_PIN 3
 #define MY_RF24_CS_PIN 4
@@ -11,35 +20,37 @@
 #define MY_SOFT_SPI_MISO_PIN 7
 #define MY_RF24_IRQ_PIN 8
 
-/* Serial setup */
+// Define USB serial speed
 #define SERIALSPEED    9600
 
-/* Ir setup */
+// Define IR serial speed and config
 #define IRSERIALSPEED 300
 #define IRSERIALCONF  SERIAL_7E1
 
-String inputString = "";         // a String to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
+String inputString = "";        // a String to hold incoming data
+String content = "";			// Content from ir serial
 
-bool poll = false;
-bool ack = false;
-bool clearserial = false;
+bool stringComplete = false;	// whether the string is complete
+bool poll = false;				// POLL flag when sending message to electric meter
+bool ack = false;				// ACK flag when sending message to electric meter
+bool clearserial = false;		// Clearing serial if needed
 
-uint32_t currentTime = 0;
-uint32_t sendFrequency = 600000;
-uint32_t lastSend = 0;
-uint32_t saveTime = 0;
-uint32_t readFrequency = 60000;
-uint32_t lastread = 0;
+uint32_t currentTime = 0;			// Actual time
+uint32_t sendFrequency = 600000;	// frequency send to MySensors
+uint32_t lastSend = 0;				// Last send time
+uint32_t readFrequency = 60000;		// frequency read electric meter 
+uint32_t lastread = 0;				// Last read electric meter time
 
-String content = "";
-char response;
-const byte maxData = 30;
-char etiquette[maxData + 1];
-char valeur[maxData + 1];
-char unite[maxData + 1];
-char message[] = "";
-char * debutPtr;
+const byte maxData = 30;    //
+char response;					// 
+char label[maxData + 1];	// 
+char value[maxData + 1];		// 
+char unit[maxData + 1];		// 
+char message[] = "";			// 
+char * ElectricMeterResponse;				// 
+
+ 
+size_t nbOfCharacter;			// Character number of the received message
 
 String VFF;   // Error code (always first in the list)
 String V181;  // Active energy import rate 1 (X = 1…6)
@@ -83,9 +94,13 @@ int V137_ID = 17;
 int V8281_ID = 18;
 int V8282_ID = 19;
 
-enum : byte { ANALYSE_OK, ERREUR_FORMAT, ERREUR_DONNEE_TROP_LONGUE } erreur;
+//array error message type
+enum : byte {
+	ANALYSE_OK,
+	ERREUR_FORMAT,
+	ERREUR_DONNEE_TROP_LONGUE
+} erreur;
 
-size_t nbCaracteres;
 
 
 #include <MySensors.h>
@@ -141,10 +156,14 @@ void presentation()
 
 }
 
+
+
 void setup() {
+
 	// initialize serial:
 	Serial.begin(SERIALSPEED);
 	Serial1.begin(IRSERIALSPEED, IRSERIALCONF);
+
 	// reserve 200 bytes for the inputString:
 	inputString.reserve(200);
 	while (!Serial) {
@@ -152,13 +171,14 @@ void setup() {
 	}
 }
 
+
+
 void loop() {
 
-	currentTime = saveTime = millis(); // Update time
+	currentTime = millis(); // Update time
 
 	// print the string when a newline arrives:
 	if (stringComplete) {
-		Serial.print(millis() / 1000);
 		Serial.print(" RECEIVE : ");
 		Serial.print(inputString);
 		// clear the string:
@@ -168,15 +188,10 @@ void loop() {
 
 	if (currentTime - lastread > readFrequency) {
 
-
-
-
-
 		if (poll == false) {
 			Serial1.begin(IRSERIALSPEED, IRSERIALCONF);
 			delay(1000);
-			Serial.print(millis() / 1000);
-			Serial.println(" SEND : /?!<CR><LF>\r\n");
+			Serial.println("IR SEND : /?!<CR><LF>\r\n");
 			Serial1.write(0xAF); // "/"  /
 			Serial1.write(0x3F); // "?"  ?
 			Serial1.write(0x21); // "!"  !
@@ -185,11 +200,12 @@ void loop() {
 
 			poll = true;
 		}
+
 		if (ack == true) {
 			Serial1.begin(IRSERIALSPEED, IRSERIALCONF);
 			delay(1000);
 			Serial.print(millis() / 1000);
-			Serial.println(F(" SEND : <ACK>000<CR><LF>\r\n"));
+			Serial.println(F("IR SEND : <ACK>000<CR><LF>\r\n"));
 			Serial1.write(0x06); // "<ACK>"
 			Serial1.write(0x30); // "0"
 			Serial1.write(0x30); // "0" 300 "1" 600
@@ -203,7 +219,6 @@ void loop() {
 	if (currentTime - lastSend > sendFrequency) {
 
 		lastSend = currentTime; // update send frequency timer
-
 
 		//send values to gateway
 		send(VFFMsg.set(VFF.toFloat(), 0));
@@ -229,17 +244,21 @@ void loop() {
 	}
 }
 
+
+
 // IR Serial event
 void serialEvent1() {
+
 	while (Serial1.available()) {
+
 		// get the new byte:
 		char inChar = (char)Serial1.read();
-		/*
-		Serial.print(F("DEBUG : int("));
-		Serial.print((int)inChar);
+
+		/* // Uncomment for debugging
 		Serial.print(F(") ASCII : "));
 		Serial.println(inChar);
 		*/
+
 		// FIND <STX>
 		if (inChar == 2) {
 			inChar = *"";
@@ -261,7 +280,6 @@ void serialEvent1() {
 			Serial1.end();
 		}
 
-
 		// if the incoming character is a newline, set a flag so the main loop can
 		// do something about it:
 		if (inChar == '\n') {
@@ -270,12 +288,9 @@ void serialEvent1() {
 
 		}
 
-		//while (Serial1.available()) {Serial1.read();}
-
 		if (inputString == "/LGZ4ZMF100AC.M29\r\n") {
 			ack = true;
 		}
-
 
 		if (clearserial == true) {
 			while (Serial1.available() > 0) { char trash = Serial1.read(); }
@@ -289,217 +304,216 @@ void serialEvent1() {
 }
 
 
+
 void decoding() {
+
 	//convert to decode
-	//Serial.println(content);
+	//Serial.println(content); // Uncomment for debugging
+
 	int str_len = content.length() + 1;
-	char debutPtr[str_len];
-	content.toCharArray(debutPtr, str_len);
+	char ElectricMeterResponse[str_len];
+	content.toCharArray(ElectricMeterResponse, str_len);
 
 	// decode and arrange
-	decodeirserial(debutPtr);
+	decodeirserial(ElectricMeterResponse);
 
 	// Clear received buffer
 	content = "";
 }
 
 
-void decodeirserial(char *debutPtr) {
 
-	while (*debutPtr) {
+void decodeirserial(char *ElectricMeterResponse) {
 
-		if (strcmp(debutPtr, "!\r\n") == 0) break; // FIN DU MESSAGE
+	while (*ElectricMeterResponse) {
+
+		if (strcmp(ElectricMeterResponse, "!\r\n") == 0) break; // END OF MESSAGE
 
 		// -------------------------------------------------------
-		// ON TROUVE LES SEPARATEURS ET ON FAIT QUELQUES VERIFS
+		// FIND SEPARATORS AND A FEW CHECKS
 		// -------------------------------------------------------
 
-		char * parentheseOuvrante = strchr(debutPtr, '(');
-		char * parentheseFermante = strchr(debutPtr, ')');
-		char * etoile = strchr(debutPtr, '*');
-		char * finDeLigne = strchr(debutPtr, '\n');
+		char * parentheseOpen = strchr(ElectricMeterResponse, '(');
+		char * parentheseClose = strchr(ElectricMeterResponse, ')');
+		char * star = strchr(ElectricMeterResponse, '*');
+		char * endOfLine = strchr(ElectricMeterResponse, '\n');
 
-		String decodage = debutPtr;
+		String decodage = ElectricMeterResponse;
 		decodage.trim();
-		//Serial.print(F("decodage de : "));
-		//Serial.println(decodage);
 
-		if ((parentheseOuvrante == NULL) ||
-			(parentheseFermante == NULL) ||
-			(finDeLigne == NULL) ||
-			(parentheseOuvrante > parentheseFermante))
+		//Serial.print(F("decodage of : ")); // Uncomment for debugging
+		//Serial.println(decodage); // Uncomment for debugging
+
+		if ((parentheseOpen == NULL) ||
+			(parentheseClose == NULL) ||
+			(endOfLine == NULL) ||
+			(parentheseOpen > parentheseClose))
 		{
-			Serial.print("ERREUR_FORMAT reçu : ");
-			Serial.println(debutPtr);
+			Serial.print("ERROR FORMAT : ");
+			Serial.println(ElectricMeterResponse);
 
 			break;
 		}
 
-		nbCaracteres = parentheseOuvrante - debutPtr;
+		nbOfCharacter = parentheseOpen - ElectricMeterResponse;
 
-		if (nbCaracteres > maxData) {
-			erreur = ERREUR_DONNEE_TROP_LONGUE;
-			Serial.println("ERREUR_DONNEE_TROP_LONGUE");
+		if (nbOfCharacter > maxData) {
+			Serial.println("ERROR DATA TOO LONG");
 			break;
 		}
 
-		strncpy(etiquette, debutPtr, nbCaracteres);
-		etiquette[nbCaracteres] = '\0';
+		strncpy(label, ElectricMeterResponse, nbOfCharacter);
+		label[nbOfCharacter] = '\0';
 
 		// -------------------------------------------------------
-		// ON SAUTE LA PARENTHESE OUVRANTE, ON LIT LES DONNEES
+		// JUMP THE OPENING PARENTHESE AND READ THE DATA
 		// -------------------------------------------------------
 
-		debutPtr = parentheseOuvrante + 1;
+		ElectricMeterResponse = parentheseOpen + 1;
 
-		// LES DONNES SE TERMINENT SOIT PAR UNE PARENTHESE FERMANTE, SOIT PAR UNE *
+		// THE DATA END EITHER BY A FERMENTED PARENTHESE, OR BY A *
 
-		if ((etoile == NULL) || (etoile > finDeLigne)) {
-			// On est dans le cas où il n'y as pas d'unités
-			nbCaracteres = parentheseFermante - debutPtr;
+		if ((star == NULL) || (star > endOfLine)) {
+			// We are in the case where there are no units
+			nbOfCharacter = parentheseClose - ElectricMeterResponse;
 
-			if (nbCaracteres > maxData) {
-				erreur = ERREUR_DONNEE_TROP_LONGUE;
-				Serial.println("ERREUR_DONNEE_TROP_LONGUE");
+			if (nbOfCharacter > maxData) {
+				Serial.println("ERROR DATA TOO LONG");
 				break;
 			}
 
-			strncpy(valeur, debutPtr, nbCaracteres);
-			valeur[nbCaracteres] = '\0';
+			strncpy(value, ElectricMeterResponse, nbOfCharacter);
+			value[nbOfCharacter] = '\0';
 
-			unite[0] = '\0'; // pas d'unités
+			unit[0] = '\0'; // no unit
 
 		}
 		else {
-			// il y a des unités
-			nbCaracteres = etoile - debutPtr;
+			// unit
+			nbOfCharacter = star - ElectricMeterResponse;
 
-			if (nbCaracteres > maxData) {
-				erreur = ERREUR_DONNEE_TROP_LONGUE;
-				Serial.println("ERREUR_DONNEE_TROP_LONGUE");
+			if (nbOfCharacter > maxData) {
+				Serial.println("ERROR DATA TOO LONG");
 				break;
 			}
 
-			strncpy(valeur, debutPtr, nbCaracteres);
-			valeur[nbCaracteres] = '\0';
+			strncpy(value, ElectricMeterResponse, nbOfCharacter);
+			value[nbOfCharacter] = '\0';
 
-			// on saute l'étoile
-			debutPtr = etoile + 1;
+			// Jump *
+			ElectricMeterResponse = star + 1;
 
-			nbCaracteres = parentheseFermante - debutPtr;
+			nbOfCharacter = parentheseClose - ElectricMeterResponse;
 
-			if (nbCaracteres > maxData) {
-				erreur = ERREUR_DONNEE_TROP_LONGUE;
-				Serial.println("ERREUR_DONNEE_TROP_LONGUE");
+			if (nbOfCharacter > maxData) {
+				Serial.println("ERROR DATA TOO LONG");
 				break;
 			}
 
-			strncpy(unite, debutPtr, nbCaracteres);
-			unite[nbCaracteres] = '\0';
+			strncpy(unit, ElectricMeterResponse, nbOfCharacter);
+			unit[nbOfCharacter] = '\0';
 		}
 
-		// On passe au champs suivant
-		debutPtr = finDeLigne + 1;
-		//Serial.print(millis()/1000);
-		//Serial.print(F(" ETIQUETTE = \"")); Serial.print(etiquette);
+		// We move to the next field
+		ElectricMeterResponse = endOfLine + 1;
 
+		//Serial.print(F(" label = \"")); Serial.print(label); // Uncomment for debugging
 
+		if ((String)label == "F.F") { VFF = (String)value; }
+		if ((String)label == "1.8.1") { V181 = (String)value; }
+		if ((String)label == "1.8.2") { V182 = (String)value; }
+		if ((String)label == "8.8.1") { V881 = (String)value; }
+		if ((String)label == "8.8.2") { V882 = (String)value; }
+		if ((String)label == "1.8.0") { V180 = (String)value; }
+		if ((String)label == "8.8.0") { V880 = (String)value; }
+		if ((String)label == "C.7.0") { VC70 = (String)value; }
+		if ((String)label == "32.7") { V327 = (String)value; }
+		if ((String)label == "52.7") { V527 = (String)value; }
+		if ((String)label == "72.7") { V727 = (String)value; }
+		if ((String)label == "31.7") { V317 = (String)value; }
+		if ((String)label == "51.7") { V517 = (String)value; }
+		if ((String)label == "71.7") { V717 = (String)value; }
+		if ((String)label == "33.7") { V337 = (String)value; }
+		if ((String)label == "53.7") { V537 = (String)value; }
+		if ((String)label == "73.7") { V737 = (String)value; }
+		if ((String)label == "13.7") { V137 = (String)value; }
+		if ((String)label == "82.8.1") { V8281 = (String)value; }
+		if ((String)label == "82.8.2") {
+			V8282 = (String)value;
 
-		if ((String)etiquette == "F.F") { VFF = (String)valeur; }
-		if ((String)etiquette == "1.8.1") { V181 = (String)valeur; }
-		if ((String)etiquette == "1.8.2") { V182 = (String)valeur; }
-		if ((String)etiquette == "8.8.1") { V881 = (String)valeur; }
-		if ((String)etiquette == "8.8.2") { V882 = (String)valeur; }
-		if ((String)etiquette == "1.8.0") { V180 = (String)valeur; }
-		if ((String)etiquette == "8.8.0") { V880 = (String)valeur; }
-		if ((String)etiquette == "C.7.0") { VC70 = (String)valeur; }
-		if ((String)etiquette == "32.7") { V327 = (String)valeur; }
-		if ((String)etiquette == "52.7") { V527 = (String)valeur; }
-		if ((String)etiquette == "72.7") { V727 = (String)valeur; }
-		if ((String)etiquette == "31.7") { V317 = (String)valeur; }
-		if ((String)etiquette == "51.7") { V517 = (String)valeur; }
-		if ((String)etiquette == "71.7") { V717 = (String)valeur; }
-		if ((String)etiquette == "33.7") { V337 = (String)valeur; }
-		if ((String)etiquette == "53.7") { V537 = (String)valeur; }
-		if ((String)etiquette == "73.7") { V737 = (String)valeur; }
-		if ((String)etiquette == "13.7") { V137 = (String)valeur; }
-		if ((String)etiquette == "82.8.1") { V8281 = (String)valeur; }
-		if ((String)etiquette == "82.8.2") {
-			V8282 = (String)valeur;
+			/* Uncomment for debugging
+			Serial.print("FF : ");
+			Serial.println(VFF);
+			Serial.print("V181 : ");
+			Serial.println(V181);
+			Serial.print("V182 : ");
+			Serial.println(V182);
+			Serial.print("V881 : ");
+			Serial.println(V881);
+			Serial.print("V882 : ");
+			Serial.println(V882);
+			Serial.print("V180 : ");
+			Serial.println(V180);
+			Serial.print("V880 : ");
+			Serial.println(V880);
+			Serial.print("VC70 : ");
+			Serial.println(VC70);
+			Serial.print("V327 : ");
+			Serial.println(V327);
+			Serial.print("V527 : ");
+			Serial.println(V527);
+			Serial.print("V727 : ");
+			Serial.println(V727);
+			Serial.print("V317 : ");
+			Serial.println(V317);
+			Serial.print("V517 : ");
+			Serial.println(V517);
+			Serial.print("V717 : ");
+			Serial.println(V717);
+			Serial.print("V337 : ");
+			Serial.println(V337);
+			Serial.print("V537 : ");
+			Serial.println(V537);
+			Serial.print("V737 : ");
+			Serial.println(V737);
+			Serial.print("V137 : ");
+			Serial.println(V137);
+			Serial.print("V8281 : ");
+			Serial.println(V8281);
+			Serial.print("V8282 : ");
+			Serial.println(V8282);
+			*/
 
-			/*
-				  Serial.print("FF : ");
-				  Serial.println(VFF);
-				  Serial.print("V181 : ");
-				  Serial.println(V181);
-				  Serial.print("V182 : ");
-				  Serial.println(V182);
-				  Serial.print("V881 : ");
-				  Serial.println(V881);
-				  Serial.print("V882 : ");
-				  Serial.println(V882);
-				  Serial.print("V180 : ");
-				  Serial.println(V180);
-				  Serial.print("V880 : ");
-				  Serial.println(V880);
-				  Serial.print("VC70 : ");
-				  Serial.println(VC70);
-				  Serial.print("V327 : ");
-				  Serial.println(V327);
-				  Serial.print("V527 : ");
-				  Serial.println(V527);
-				  Serial.print("V727 : ");
-				  Serial.println(V727);
-				  Serial.print("V317 : ");
-				  Serial.println(V317);
-				  Serial.print("V517 : ");
-				  Serial.println(V517);
-				  Serial.print("V717 : ");
-				  Serial.println(V717);
-				  Serial.print("V337 : ");
-				  Serial.println(V337);
-				  Serial.print("V537 : ");
-				  Serial.println(V537);
-				  Serial.print("V737 : ");
-				  Serial.println(V737);
-				  Serial.print("V137 : ");
-				  Serial.println(V137);
-				  Serial.print("V8281 : ");
-				  Serial.println(V8281);
-				  Serial.print("V8282 : ");
-				  Serial.println(V8282);
+			//send values to gateway
+			send(VFFMsg.set(VFF.toFloat(), 0));
+			send(V181Msg.set(V181.toFloat(), 3));
+			send(V182Msg.set(V182.toFloat(), 3));
+			send(V881Msg.set(V881.toFloat(), 3));
+			send(V882Msg.set(V882.toFloat(), 3));
+			send(V180Msg.set(V180.toFloat(), 3));
+			send(V880Msg.set(V880.toFloat(), 3));
+			send(VC70Msg.set(VC70.toFloat(), 0));
+			send(V327Msg.set(V327.toFloat(), 3));
+			send(V527Msg.set(V527.toFloat(), 3));
+			send(V727Msg.set(V727.toFloat(), 3));
+			send(V317Msg.set(V317.toFloat(), 3));
+			send(V517Msg.set(V517.toFloat(), 3));
+			send(V717Msg.set(V717.toFloat(), 3));
+			send(V337Msg.set(V337.toFloat(), 3));
+			send(V537Msg.set(V537.toFloat(), 3));
+			send(V737Msg.set(V737.toFloat(), 3));
+			send(V137Msg.set(V137.toFloat(), 3));
+			send(V8281Msg.set(V8281.toFloat(), 0));
+			send(V8282Msg.set(V8282.toFloat(), 0));
 
-				  //send values to gateway
-				send(VFFMsg.set(VFF.toFloat(),0));
-				send(V181Msg.set(V181.toFloat(),3));
-				send(V182Msg.set(V182.toFloat(),3));
-				send(V881Msg.set(V881.toFloat(),3));
-				send(V882Msg.set(V882.toFloat(),3));
-				send(V180Msg.set(V180.toFloat(),3));
-				send(V880Msg.set(V880.toFloat(),3));
-				send(VC70Msg.set(VC70.toFloat(),0));
-				send(V327Msg.set(V327.toFloat(),3));
-				send(V527Msg.set(V527.toFloat(),3));
-				send(V727Msg.set(V727.toFloat(),3));
-				send(V317Msg.set(V317.toFloat(),3));
-				send(V517Msg.set(V517.toFloat(),3));
-				send(V717Msg.set(V717.toFloat(),3));
-				send(V337Msg.set(V337.toFloat(),3));
-				send(V537Msg.set(V537.toFloat(),3));
-				send(V737Msg.set(V737.toFloat(),3));
-				send(V137Msg.set(V137.toFloat(),3));
-				send(V8281Msg.set(V8281.toFloat(),0));
-				send(V8282Msg.set(V8282.toFloat(),0));
-				*/
 		}
 
-		//Serial.print(F("\", VALEUR = \"")); Serial.print(valeur);
+		//Serial.print(F("\", value = \"")); Serial.print(value); // Uncomment for debugging
 
-		if (unite[0] != '\0') { // si on avait des unités
-		  //Serial.print(F("\", UNITE = \"")); Serial.print(unite); Serial.println(F("\""));
+		if (unit[0] != '\0') { // si on avait des unités
+		  //Serial.print(F("\", unit = \"")); Serial.print(unit); Serial.println(F("\"")); // Uncomment for debugging
 		}
-	} // FIN DU WHILE
+	} // End of while
 
 	lastread = currentTime; // update send frequency timer
-
 }
